080b32905c359d6e7f4596d765d620f294e56c43
Nelio Alves <acenelio@gmail.com>
2018-08-05 10:30:07 -0300

    Variable to lambda expression

--- Course/Program.cs
+++ Course/Program.cs
@@ -14,16 +14,12 @@ namespace Course {
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Func<Product, string> func = NameUpper;
+            Func<Product, string> func = p => p.Name.ToUpper();
 
             List<string> result = list.Select(func).ToList();
             foreach (string s in result) {
                 Console.WriteLine(s);
             }
-        }
-
-        static string NameUpper(Product p) {
-            return p.Name.ToUpper();
         }
     }
 }
