7706ebdb3f51c88b9a1b539931c962b438a779fd
Nelio Alves <acenelio@gmail.com>
2018-08-05 10:29:14 -0300

    Variable to reference method

--- Course/Program.cs
+++ Course/Program.cs
@@ -14,7 +14,9 @@ namespace Course {
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            List<string> result = list.Select(NameUpper).ToList();
+            Func<Product, string> func = NameUpper;
+
+            List<string> result = list.Select(func).ToList();
             foreach (string s in result) {
                 Console.WriteLine(s);
             }
