1be771ee9424787b5bb645488f1c05eaf972f140
Nelio Alves <acenelio@gmail.com>
2018-08-05 10:31:03 -0300

    Inline lambda expression

--- Course/Program.cs
+++ Course/Program.cs
@@ -14,9 +14,7 @@ namespace Course {
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Func<Product, string> func = p => p.Name.ToUpper();
-
-            List<string> result = list.Select(func).ToList();
+            List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
             foreach (string s in result) {
                 Console.WriteLine(s);
             }
